04eb9e64acf99cc88e603b6622352f477cdf116b
reshu1726 <128213197+reshu1726@users.noreply.github.com>
2023-04-19 12:34:02 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+ @Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.gradle.versions)
+    alias(libs.plugins.version.catalog.update)
+}
+
+apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
